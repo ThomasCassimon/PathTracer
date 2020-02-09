664a4d7a573db42faaa096bd503d6e5d031d05d3
Thomas Cassimon <thomas.cassimon@outlook.com>
2020-02-09 22:08:17 +0100

    Cleanup and moved to 3-element vectors.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,59 +6,20 @@
 #include <iostream>
 #include <random>
 
-#include <vector_buffer.hpp>
+#include <vec_3.hpp>
 
 using namespace raytracer;
 
-void performance()
-{
-	const std::size_t SIZE = 10000000;
-
-	vector_buffer<vec4_type<>, aos_layout> buffer;
-
-	buffer.data.push_back(loadv(1.0, 2.0, 3.0, 4.0));
-
-	std::cout << normalize(buffer.data.front()) << '\n';
-
-	std::random_device dev;
-	std::uniform_real_distribution<float> dist;
-
-	for (unsigned int i = 0; i < (SIZE - 1); ++i)
-	{
-		buffer.data.push_back(loadv(dist(dev), dist(dev), dist(dev), dist(dev)));
-	}
-
-	vector_buffer<norm4_type<>, aos_layout> resultBuffer;
-	resultBuffer.data.resize(buffer.data.size());
-	const unsigned int size = buffer.data.size();
-	unsigned int i = 0;
-
-	auto start = std::chrono::high_resolution_clock::now();
-
-	for (; i < size; ++i)
-	{
-		resultBuffer.data[i] = normalize(buffer.data[i]);
-	}
-
-	auto end = std::chrono::high_resolution_clock::now();
-
-	const unsigned long duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-
-	std::cout << "Normalizing " << resultBuffer.data.size() << " 4-element vectors took "
-			  << static_cast<double>(duration) / 1000000.0 << "s\n";
-}
-
 int main()
 {
-	// performance();
-
 	const unsigned short IMAGE_WIDTH = 640;
 	const unsigned short IMAGE_HEIGHT = 480;
 
-	const vec4_type<> camera = loadv(0.0f, 1.0f, 1.0f, 0.0f);
-	const norm4_type<> normDir = normalize(loadv(0.0f, 1.0f, -1.0f, 0.0f));
+	const vec3_type<> camera = loadv(0.0f, 2.0f, 2.0f);
+	const norm3_type<> normDir = normalize(loadv(0.0f, -1.0f, -1.0f));
+	const norm3_type<> up = normalize(loadv(0.0f, 1.0f, -1.0f));
 
-	const vec4_type<> screenPos = add(camera, static_cast<vec4_type<>>(normDir));
+	const vec3_type<> screenPos = add(camera, static_cast<vec3_type<>>(normDir));
 
 	for (unsigned short i = 0; i < IMAGE_HEIGHT; ++i)
 	{
